3c161abaeb6d44ce6e11483ca773f39364ea378f
CyberBlast <ashishdantesparda@gmail.com>
2019-07-15 20:27:16 +0530

    02: Each cube tells its coordinate according to custom gridSpace.

--- RRTD/Assets/EditorSnap.cs
+++ RRTD/Assets/EditorSnap.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 
 [ExecuteInEditMode]
+[SelectionBase]
 public class EditorSnap : MonoBehaviour {
 
 	[Range(1, 20)] [SerializeField] int gridSize = 10;
+	TextMesh textMesh;
 
 	void Update()
 	{
 		Vector3 snapPos;
 		snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
 		snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
 		transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+		textMesh = GetComponentInChildren<TextMesh>();
+		textMesh.text = "(" + snapPos.x / gridSize + "," + snapPos.z / gridSize + ")";
 	}
 }
