d04ee94262dcbe1915175f0e0b30be722b8c734c
CyberBlast <ashishdantesparda@gmail.com>
2019-07-16 17:30:09 +0530

    03: Enemy now moves along the specified waypoints.

--- RRTD/Assets/EditorSnap.cs
+++ RRTD/Assets/EditorSnap.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 [SelectionBase]
+[RequireComponent(typeof(Waypoint))]
 public class EditorSnap : MonoBehaviour {
 
-	[Range(1, 20)] [SerializeField] int gridSize = 10;
-	TextMesh textMesh;
+	Waypoint waypoint;
+
+	void Awake()
+	{
+		waypoint = GetComponent<Waypoint>();
+	}
 
 	void Update()
 	{
-		Vector3 snapPos;
-		snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-		snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-		transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+		SnapToGrid();
+		UpdateLabel();
+	}
 
-		textMesh = GetComponentInChildren<TextMesh>();
-		textMesh.text = "(" + snapPos.x / gridSize + "," + snapPos.z / gridSize + ")";
+	private void SnapToGrid()
+	{
+		int gridSize = waypoint.GetGridSize();
+		transform.position = new Vector3(
+				waypoint.GetGridPos().x * waypoint.GetGridSize(),
+				0f,
+				waypoint.GetGridPos().y * waypoint.GetGridSize()
+			);
+	}
+
+	private void UpdateLabel()
+	{
+		TextMesh textMesh = GetComponentInChildren<TextMesh>();
+		int gridSize = waypoint.GetGridSize();
+		string labelText = "(" + waypoint.GetGridPos().x + "," + waypoint.GetGridPos().y + ")";
+		textMesh.text = labelText;
+		gameObject.name = labelText;
 	}
 }
