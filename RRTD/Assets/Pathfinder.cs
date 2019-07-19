cabe6df6819fb1555d2a1f3cb133a75012f443e7
CyberBlast <ashishdantesparda@gmail.com>
2019-07-19 12:05:55 +0530

    05: Implementing queues for BFS pathfinding algorithm.

--- RRTD/Assets/Pathfinder.cs
+++ RRTD/Assets/Pathfinder.cs
@@ -8,11 +8,64 @@ public class Pathfinder : MonoBehaviour {
 	[SerializeField] Waypoint start, end;
 
 	Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+	Queue<Waypoint> queue = new Queue<Waypoint>();
+	bool isRunning = true;
+
+	Vector2Int[] directions =
+	{
+		Vector2Int.up,
+		Vector2Int.right,
+		Vector2Int.down,
+		Vector2Int.left
+	};
 
 	// Use this for initialization
 	void Start () {
 		LoadAllWaypoints();
 		ColorStartAndEnd();
+		Pathfind();
+	}
+
+	private void Pathfind()
+	{
+		queue.Enqueue(start);
+		while (queue.Count > 0 && isRunning)
+		{
+			Waypoint searchCenter = queue.Dequeue();
+			print("Searching from: " + searchCenter);
+			if (searchCenter == end)
+			{
+				print("End waypoint found.");
+				isRunning = false;
+				return;
+			}
+			ExploreNeighbors(searchCenter);
+			searchCenter.isExplored = true;
+		}
+	}
+
+	private void ExploreNeighbors(Waypoint from)
+	{
+		if (isRunning == false) return;
+
+		Vector2Int neighborCoords;
+		foreach (Vector2Int direction in directions)
+		{
+			neighborCoords = from.GetGridPos() + direction;
+			if (grid.ContainsKey(neighborCoords))
+			{
+				QueueNewNeighbor(neighborCoords);
+			}
+		}
+	}
+
+	private void QueueNewNeighbor(Vector2Int neighborCoords)
+	{
+		Waypoint neighbor = grid[neighborCoords];
+		if (neighbor.isExplored) return;
+		neighbor.SetWaypointColor(Color.yellow);
+		queue.Enqueue(neighbor);
+		print("Queueing: " + neighborCoords);
 	}
 
 	private void ColorStartAndEnd()
@@ -33,11 +86,7 @@ public class Pathfinder : MonoBehaviour {
 			}
 			grid.Add(waypoint.GetGridPos(), waypoint);
 		}
-		print(grid.Count);
+		print("Loaded " + grid.Count + " Waypoints");
 	}
-
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	
 }
