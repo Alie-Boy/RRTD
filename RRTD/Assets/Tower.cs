294df9396ee600160fba2c77bbdcdd4b495d9c53
CyberBlast <ashishdantesparda@gmail.com>
2019-07-20 11:20:23 +0530

    07: Imported new assets along with towers which look at enemy's position.

---
+++ RRTD/Assets/Tower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+	[SerializeField] Transform objectToMove;
+	[SerializeField] Transform target;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		objectToMove.LookAt(target);
+	}
+}
